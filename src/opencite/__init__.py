a94ad2058a41a43f151cf60e9cdf746cc5e60408
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-02-07 09:37:52 -0800

    Add .env.example, bump version to 0.1.1

--- src/opencite/__init__.py
+++ src/opencite/__init__.py
@@ -1,6 +1,6 @@
 """opencite: Academic literature search, citation management, and PDF retrieval."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .config import Config
 from .models import (

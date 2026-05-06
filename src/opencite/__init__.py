72ddd1a9632a357e91eaab483c317a1d8487be36
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-05-06 07:19:30 -0700

    Bump version to 0.4.0 (preprint integration epic)

--- src/opencite/__init__.py
+++ src/opencite/__init__.py
@@ -1,6 +1,6 @@
 """opencite: Academic literature search, citation management, and PDF retrieval."""
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from .config import Config
 from .models import (
